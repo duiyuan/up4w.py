69c70191f98a2a76e0aff6921ab3a8db51c8fc8e
wangshangwen <shangwenwang20@gmail.com>
2023-06-06 23:46:51 +0800

    chore: improve cytpes call dll

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # up4w.py
-A python libuary interact with UP4W 
+A python library interact with UP4W 
--- up4w/up4w-service.py
+++ up4w/up4w-service.py
@@ -1,37 +1,44 @@
-import ctypes
+from ctypes import CDLL
 import os
-from os.path import join
+import time
+from os.path import join, exists, dirname, abspath
 from sys import platform
 
 
 class Up4wService:
     def __init__(self, *, debug=False, appdata=os.getenv("APPDATA")):
         self.debug = bool(debug)
-        self.addons_path = join(os.curdir, "addons")
+        self.addons_path = abspath(join(os.curdir, "addons"))
         self.appdata = appdata
 
         # windows -> dll
         if platform == "win32":
-            self.file_path = join(self.addons_path, "up4w_core_shared.dll")
+            self.file_path = join(self.addons_path, platform, "up4w_core_shared.dll")
         # macOS -> dylib
         elif platform == "darwin":
-            self.file_path = join(self.addons_path, "up4w_core_shared.dylib")
+            self.file_path = join(self.addons_path, platform, "up4w_core_shared.dylib")
         # linux -> linux
         elif platform == "linux":
-            self.file_path = join(self.addons_path, "up4w_core_shared.so")
+            self.file_path = join(self.addons_path, platform, "up4w_core_shared.so")
         else:
             raise Exception("Unsupported platform")
 
     def start(self):
         appdata = self.appdata
-        loader = ctypes.cdll.LibraryLoader(self.file_path)
+        if not exists(self.file_path):
+            raise Exception("File does not exist :" + self.file_path)
+        print(self.file_path)
+        loader = CDLL(self.file_path)
         start = loader.start(f"-data={appdata}")
+        port = loader.get_api_port()
         print(start)
+        print(port)
 
 
 if __name__ == "__main__":
     server = Up4wService()
     server.start()
+    time.sleep(1000)
 
 
 
