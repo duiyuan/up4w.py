254d39720f63403286aaaf830a34cef1a371dcb5
wangshangwen <shangwenwang20@gmail.com>
2023-06-07 14:07:25 +0800

    chore: pip child process

--- up4w/up4w-service.py
+++ up4w/up4w-service.py
@@ -1,8 +1,23 @@
 from ctypes import CDLL
 import os
 import time
+from multiprocessing import Process, Pipe
+from json import dumps, loads
 from os.path import join, exists, dirname, abspath
 from sys import platform
+
+
+fd1, fd2 = Pipe()
+
+
+def start_up4w(fd, apppath, dll_path):
+    loader = CDLL(dll_path)
+    start = loader.start(f"-data={apppath}")
+    port = loader.get_api_port()
+    fd.send(dumps({"port": port, "ret": start, "pid": os.getpid()}))
+    # The dynamic link library will run a websocket service
+    # Keep the process alive, prevent the gc from collecting it
+    # time.sleep(3600 * 24 * 365 * 10)
 
 
 class Up4wService:
@@ -23,22 +38,30 @@ class Up4wService:
         else:
             raise Exception("Unsupported platform")
 
+    @staticmethod
+    def recv(fd):
+        data = fd.recv()
+        return data
+
     def start(self):
         appdata = self.appdata
         if not exists(self.file_path):
             raise Exception("File does not exist :" + self.file_path)
         print(self.file_path)
-        loader = CDLL(self.file_path)
-        start = loader.start(f"-data={appdata}")
-        port = loader.get_api_port()
-        print(start)
-        print(port)
+
+        child = Process(target=start_up4w, args=(fd1, appdata, self.file_path))
+        # parent = Process(target=self.recv, args=(fd2,))
+        child.start()
+        # parent.start()
+        child.join()
+        # parent.join()
+        return self.recv(fd2)
 
 
 if __name__ == "__main__":
     server = Up4wService()
-    server.start()
-    time.sleep(1000)
+    ep = server.start()
+    print(ep)
 
 
 
