3ee57ec4cd133cdde2bbb058bd916232854117b6
wangshangwen <shangwenwang20@gmail.com>
2023-06-10 15:29:08 +0800

    chore: preconfig xxx.nodes to appdata path

--- up4w/up4w-service.py
+++ up4w/up4w-service.py
@@ -1,6 +1,7 @@
 from ctypes import CDLL
 import os
 import time
+import shutil
 from multiprocessing import Process, Pipe, Event
 from json import dumps, loads
 from os.path import join, exists, dirname, abspath
@@ -31,6 +32,7 @@ class Up4wService:
     def __init__(self, *, debug=False, appdata=os.getenv("APPDATA")):
         self.debug = bool(debug)
         self.addons_path = abspath(join(os.curdir, "addons"))
+        self.nodes_path = abspath(join(os.curdir, "nodes"))
         self.appdata = appdata
         self.child = None
 
@@ -39,21 +41,30 @@ class Up4wService:
 
         # windows -> dll
         if platform == "win32":
-            self.file_path = join(self.addons_path, platform, "up4w_core_shared.dll")
+            self.file_path = join(self.addons_path, platform, "up4w.dll")
         # macOS -> dylib
         elif platform == "darwin":
-            self.file_path = join(self.addons_path, platform, "up4w_core_shared.dylib")
+            self.file_path = join(self.addons_path, platform, "up4w.dylib")
         # linux -> linux
         elif platform == "linux":
-            self.file_path = join(self.addons_path, platform, "up4w_core_shared.so")
+            self.file_path = join(self.addons_path, platform, "up4w.so")
         else:
             raise Exception("Unsupported platform")
+
+    def preset_nodes(self):
+        nodes_path = self.nodes_path
+        with os.scandir(nodes_path) as files:
+            for file in files:
+                file_type = file.name.split(".")[1]
+                if file_type == "nodes":
+                    shutil.copy2(file, self.appdata)
 
     def start(self):
         appdata = self.appdata
         if not exists(self.file_path):
             raise Exception("File does not exist :" + self.file_path)
 
+        self.preset_nodes()
         self.child = Process(target=start_up4w, name="up4w", args=(self.child_conn, appdata, self.file_path))
         self.child.start()
 
